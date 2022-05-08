7d5952ac4a581244ffbe3f6d29d9f1079771eba6
Aliaksei Pankratau <super.ponkrat2014@yandex.ru>
2022-05-08 19:06:29 +0300

    Fill model

--- src/app/models/plan.model.ts
+++ src/app/models/plan.model.ts
@@ -1,2 +1,18 @@
+import { Machine } from "./machine.model";
+import { Operation } from "./operation.model";
+import { Order } from "./order.model";
+
 export class Plan {
+    planId!: number;
+    plannedDate!: Date;
+    completionStatus!: string;
+    operationByOperationId!: Operation;
+    orderByOrderId!: Order;
+    machineByMachineId!: Machine;
+
+    constructor() {
+        this.operationByOperationId = new Operation();
+        this.orderByOrderId = new Order();
+        this.machineByMachineId = new Machine();
+    }
 }
--- src/app/models/user-plan.model.ts
+++ src/app/models/user-plan.model.ts
@@ -4,4 +4,9 @@ import { User } from "./user.model";
 export class UserPlan {
     planByPlanId!: Plan;
     userByUserId!: User;
+
+    constructor() {
+        this.planByPlanId = new Plan();
+        this.userByUserId = new User();
+    }
 }
